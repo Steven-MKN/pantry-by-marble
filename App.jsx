3d24db78ba9ecd9cbcf3e7da2a3ae7a4b2af64c3
Steven Mokoena <steven@synthesis.co.za>
2025-01-29 06:11:01 +0200

    fix font load

--- App.jsx
+++ App.jsx
@@ -1,8 +1,32 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { registerRootComponent } from "expo";
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 import Main from './app/Main';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [loaded, error] = useFonts({
+    "Avenir": require("./assets/fonts/Avenir.ttf"),
+    "AGaramondPro-Regular": require("./assets/fonts/AGaramondPro-Regular.otf"),
+    "AGaramondPro-Bold": require("./assets/fonts/AGaramondPro-Bold.otf"),
+    "AGaramondPro-BoldItalic": require("./assets/fonts/AGaramondPro-BoldItalic.otf"),
+    "AGaramondPro-Italic": require("./assets/fonts/AGaramondPro-Italic.otf"),
+    "AGaramondPro-SemiboldItalics": require("./assets/fonts/AGaramondPro-SemiboldItalic.otf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <Main />
--- app/sceens/auth/Login.tsx
+++ app/sceens/auth/Login.tsx
@@ -1,9 +1,11 @@
-import { Text, View } from 'react-native';
+
+import Label from '@/app/components/Label';
+import { View } from 'react-native';
 
 export default function Login() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Login</Text>
+      <Label varient='screen-heading-italic'>Welcome to Pantry by Marble</Label>
     </View>
   )
 }
